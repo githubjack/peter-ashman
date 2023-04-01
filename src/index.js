609cd0bfe978cb21502a7e13fb6ec3c2ab2cc8de
githubjack <jack.ui.maynard@gmail.com>
2023-04-01 21:50:48 +0100

    3 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import './reset.css';
+// import '/fonts/Inter-Medium.woff';
+// import '/fonts/Inter-Regular.woff';
+// import '/fonts/Jost-Regular.woff';
+// import '/fonts/Jost-SemiBold.woff';
 import App from './App';
 
 const rootElement = document.getElementById('root');
